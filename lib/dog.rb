8a1b17da478ee4b847fb9b08e7ac976b9156724b
Kitty Davies <kittyfdavies@gmail.com>
2018-03-26 21:46:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,21 @@
 class Dog
   @@all = []
+  
+  attr_accessor :name
+  
   def initialize(name)
     @name = name
     @@all << name
   end
+  
   def self.clear_all
     @@all = []
   end
+  
   def self.all
     @@all.each do |dog|
       puts dog
     end
   end
+  
 end
